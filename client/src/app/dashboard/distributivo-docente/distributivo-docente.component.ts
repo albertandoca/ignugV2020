13d147d8c7bf7f82b28b5b022d1fc51b542b22ad
albertandoca <albertandoca@gmail.com>
2020-04-30 00:33:11 -0500

    dp

--- client/src/app/dashboard/distributivo-docente/distributivo-docente.component.ts
+++ client/src/app/dashboard/distributivo-docente/distributivo-docente.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { ServerService } from './../../servicios/server.service';
 import { Instituto } from './../../modelos/instituto';
 import { Asignatura } from './../../modelos/asignatura';
@@ -55,22 +56,31 @@ export class DistributivoDocenteComponent implements OnInit {
   institutoSeleccionado: number;
   institutos: Array<Instituto>;
   url: string;
-  ultimoIds: Array<number>;
+  ultimoIds: Array<any>;
   textoAyuda: string;
   tituloFiltro: string;
   contenedorAsignatura: Array<Array<any>>;
   docentes: Array<any>;
   docentesOrg: Array<Persona>;
   docentesAsignaturas: Array<DocenteAsignatura>;
-  docenteAsignatura: DocenteAsignatura;
+  guardarDocentesAsignaturas: Array<DocenteAsignatura>;
   idCarrera: number;
+  idMalla: number;
+  idPeriodoAcademico: number;
   iconoChart: Array<boolean>;
+  iconoGuardaDocenteAsignatura: Array<boolean>;
   colorDocente: string;
   verAsignatura: boolean;
+  idJornada: number;
+  idParalelo: number;
+  iChart: number;
+  ordHora: boolean;
+  ordNombre: boolean;
 
   constructor(
     private apiService: ApiService,
     private autorizado: AutorizadoService,
+    private toastr: ToastrService,
     private serverService: ServerService
   ) {
     monkeyPatchChartJsTooltip();
@@ -82,6 +92,9 @@ export class DistributivoDocenteComponent implements OnInit {
     this.docentesOrg = [];
     this.docentes = [];
     this.iconoChart = [];
+    this.ultimoIds = [[], [], [], []];
+    this.ordHora = true;
+    this.ordNombre = true;
     this.verAsignatura = false;
     this.url = this.serverService.getUrl();
     this.vistaChart = 0;  // valor de incio dado rolUsuario
@@ -98,7 +111,7 @@ export class DistributivoDocenteComponent implements OnInit {
   async periodoLectivoActivo() {
     const res = await this.apiService.sendApi('periodo-lectivo-activo');
     this.periodoLectivo = res[0] || null;
-    this.dataChart(this.ids, true);
+    this.dataChart(this.ids);
   }
 
   async leerInstitutos(ids: Array<number>) {
@@ -118,8 +131,9 @@ export class DistributivoDocenteComponent implements OnInit {
 
   }
 
-  async dataChart(ids: number[], bandera: boolean, idPA?: number) {
-    this.ultimoIds = ids;
+  async dataChart(ids: number[], idPA?: number) {
+    console.log(this.ultimoIds);
+    console.log(this.vistaChart);
     this.verAsignatura = false;
     this.charts = [];
     const datos = {
@@ -132,36 +146,43 @@ export class DistributivoDocenteComponent implements OnInit {
     };
 
     if (this.vistaChart === 0 && this.nivelRol === 'Instituto') {       // Carreras
-      console.log('uno');
+      this.ultimoIds[0] = ids;
       datos.grupo = 2;
       datos.idsInstitutos = ids;
     } else if (this.vistaChart === 0 && this.nivelRol === 'Carrera') {
+      this.ultimoIds[0] = ids;
       datos.grupo = 2;
       datos.idsCarreras = ids;
       this.tituloFiltro = 'CARRERAS';
     }
     if (this.vistaChart === 1) {  // Mallas
+      this.ultimoIds[1] = ids;
       datos.grupo = 1;
       datos.idsCarreras = ids;
       this.idCarrera = ids[0];
       this.tituloFiltro = 'CARRERA MALLA';
     }
     if (this.vistaChart === 2) { // periodo académicos
+      this.ultimoIds[2] = ids;
       datos.grupo = 0;
       datos.idMalla = ids[0];
       this.tituloFiltro = 'CARRERAS MALLA PA';
+      console.log('llego 2');
     }
     if (this.vistaChart === 3) {  // paralelos Jornadas
+      this.ultimoIds[3] = ids;
       datos.grupo = 0;
       datos.idMalla = ids[0];
       datos.idPeriodoAcademico = idPA;
     }
-    console.log(datos);
-    if (this.vistaChart <= 2) {
+    if (this.vistaChart < 3) {
       this.totalAsignaturas = await this.apiService.sendApi('contar-asignaturas', datos);
+      console.log(this.totalAsignaturas);
       this.totalDocentesAsignaturas = await this.apiService.sendApi('contar-docentes-asignaturas', datos);
+      console.log(this.totalDocentesAsignaturas);
     }
     if (this.vistaChart === 3) {
+
       this.totalAsignaturas = await this.apiService.sendApi('leer-periodos-academicos-paralelos', datos);
       this.totalDocentesAsignaturas = await this.apiService.sendApi('contar-docentes-asignaturas-paralelos', datos);
       this.asignaturas = await this.apiService.sendApi('leer-asignaturas-periodo-academico', datos);
@@ -196,6 +217,7 @@ export class DistributivoDocenteComponent implements OnInit {
     }
 
     if (this.vistaChart <= 1) {
+
       for (const total of this.totalAsignaturas) {
         let banderaAux = true;
         for (const totalDocente of this.totalDocentesAsignaturas) {
@@ -217,6 +239,7 @@ export class DistributivoDocenteComponent implements OnInit {
         }
       }
     } else if (this.vistaChart === 2) {
+
       for (const total of this.totalAsignaturas) {
         let banderaAux = true;
         for (const totalDocente of this.totalDocentesAsignaturas) {
@@ -242,10 +265,11 @@ export class DistributivoDocenteComponent implements OnInit {
     }
     this.vistaChart++;
     console.log(this.vistaChart);
-    console.log(this.charts);
   }
 
   async asignarDocentesParalelos(idM, idPA, idP, idJ, iChart) {
+    this.iconoGuardaDocenteAsignatura = [];
+    this.guardarDocentesAsignaturas = [];
     if (this.iconoChart[iChart]) {
       this.verAsignatura = true;
       this.contenedorAsignatura = [];
@@ -256,7 +280,11 @@ export class DistributivoDocenteComponent implements OnInit {
         idParalelo: idP,
         idPeriodoLectivo: this.periodoLectivo.id
       };
-
+      this.iChart = iChart;
+      this.idMalla = idM;
+      this.idPeriodoAcademico = idPA;
+      this.idJornada = idJ;
+      this.idParalelo = idP;
       this.docentesOrg = await this.apiService.sendApi('leer-docentes-carreras', this.idCarrera);
       this.docentes = this.docentesOrg;
       await this.contarHorasDocente();
@@ -264,6 +292,8 @@ export class DistributivoDocenteComponent implements OnInit {
 
       let contador = 0;
       for (const asignatura of this.asignaturas) {
+        this.iconoGuardaDocenteAsignatura.push(true);
+        this.guardarDocentesAsignaturas.push();
         let bandera = true;
         this.contenedorAsignatura[contador] = [];
         for (const docente of this.docentesAsignaturas) {
@@ -300,7 +330,7 @@ export class DistributivoDocenteComponent implements OnInit {
     }
   }
 
-  async horasDocente(id: number): Promise<number>{
+  async horasDocente(id: number): Promise<number> {
     const horas = await this.docentes.find(docente => docente.id === id);
     return horas.horas;
   }
@@ -319,13 +349,20 @@ export class DistributivoDocenteComponent implements OnInit {
 
   dataChartInstituto() {
     this.vistaChart = 0;
-    this.dataChart([this.institutoSeleccionado], true);
+    this.dataChart([this.institutoSeleccionado]);
   }
 
   backDataChart() {
-    this.vistaChart -= 2;
-    this.tituloFiltro = '';
-    this.dataChart(this.ultimoIds, true);
+    if (this.vistaChart === 4) {
+      this.vistaChart = 2;
+      this.dataChart(this.ultimoIds[2]);
+    } else if (this.vistaChart === 3) {
+      this.vistaChart = 1;
+      this.dataChart(this.ultimoIds[1]);
+    } else if (this.vistaChart === 2) {
+      this.vistaChart = 0;
+      this.dataChart(this.ultimoIds[0]);
+    }
   }
 
   verDocentes() {
@@ -333,13 +370,20 @@ export class DistributivoDocenteComponent implements OnInit {
   }
 
   txtAyuda() {
-    this.textoAyuda = 'Debemos colocar texto ayuda por cada pantalla';
+    this.textoAyuda = 'Colocar instrucciones en funcion de vistaChart para se vidualizadas en el contenedor';
   }
 
-  guardarDocenteAsignatura() {
-
+  async guardarDocenteAsignatura(i: number) {
+    const guardar = this.apiService.sendApi('gestionar-docente-asignatura', this.guardarDocentesAsignaturas[i]);
+    if (guardar) {
+      this.toastr.success('Registro guardado', 'Guardar OK');
+      this.iconoGuardaDocenteAsignatura[i] = true;
+      //this.asignarDocentesParalelos(this.idMalla, this.idPeriodoAcademico, this.idParalelo, this.idJornada, this.iChart);
+      this.guardarDocentesAsignaturas[i] = null;
+    } else {
+      this.toastr.error('Registro guardado', 'Guardar OK')
+    }
   }
-
 
   drop(event: CdkDragDrop<number[]>, i: number) {
     if (event.container.data.length > 0) {
@@ -353,21 +397,63 @@ export class DistributivoDocenteComponent implements OnInit {
         event.previousIndex,
         event.currentIndex);
     }
-    console.log(this.contenedorAsignatura[i][0]);
-    console.log(event);
+    let id = null;
+
     if (event.previousContainer.id === 'docentes') {
+      id = i;
       this.contenedorAsignatura[i][0].horas =  parseInt(this.contenedorAsignatura[i][0].horas) + this.asignaturas[i].creditos;
+      this.iconoGuardaDocenteAsignatura[i] = false;
     } else {
-      const id = parseInt(event.previousContainer.id);
+      id = parseInt(event.previousContainer.id);
       this.contenedorAsignatura[i][0].horas =  parseInt(this.contenedorAsignatura[i][0].horas) +
                                                this.asignaturas[i].creditos - this.asignaturas[id].creditos;
-
+      if (this.iconoGuardaDocenteAsignatura[id]) {
+        this.iconoGuardaDocenteAsignatura[i] = false;
+        this.iconoGuardaDocenteAsignatura[id] = false;
+        for (const docente of this.docentesAsignaturas) {
+          if (docente.idAsignatura === this.asignaturas[id].id &&
+            docente.idJornada === this.idJornada &&
+            docente.idParalelo === this.idParalelo) {
+              this.guardarDocentesAsignaturas[id] = docente;
+              this.guardarDocentesAsignaturas[id].estado = false;
+              delete this.guardarDocentesAsignaturas[id].Persona;
+              delete this.guardarDocentesAsignaturas[id].Asignatura;
+            }
+          break;
+        }
+      } else {
+        this.iconoGuardaDocenteAsignatura[i] = false;
+        this.iconoGuardaDocenteAsignatura[id] = true;
+      }
+    }
+    let bandera = true;
+    for (const docente of this.docentesAsignaturas) {
+      if (docente.idAsignatura === this.asignaturas[i].id &&
+        docente.idJornada === this.idJornada &&
+        docente.idParalelo === this.idParalelo) {
+          this.guardarDocentesAsignaturas[i] = docente;
+          delete this.guardarDocentesAsignaturas[i].Persona;
+          delete this.guardarDocentesAsignaturas[i].Asignatura;
+          bandera = false;
+        }
+      break;
+    }
+    if (bandera) {
+      this.guardarDocentesAsignaturas[i] = {
+        id: null,
+        idDocente: this.contenedorAsignatura[i][0].id,
+        idAsignatura: this.asignaturas[i].id,
+        idPeriodoLectivo: this.periodoLectivo.id,
+        idJornada: this.idJornada,
+        idParalelo: this.idParalelo,
+        estado: true
+      };
     }
 
   }
 
   dropDocente(event: CdkDragDrop<number[]>) {
-    console.log(event);
+
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -379,24 +465,116 @@ export class DistributivoDocenteComponent implements OnInit {
     if (event.previousContainer.id !== 'docente') {
       const idOld = parseInt(event.previousContainer.id);
       const idNew = event.currentIndex;
-      console.log(idOld);
-      console.log(idNew);
       this.docentes[idNew].horas =  parseInt(this.docentes[idNew].horas) - this.asignaturas[idOld].creditos;
+
+      if (this.iconoGuardaDocenteAsignatura[idOld]) {
+        for (const docente of this.docentesAsignaturas) {
+          if (docente.idAsignatura === this.asignaturas[idOld].id &&
+            docente.idJornada === this.idJornada &&
+            docente.idParalelo === this.idParalelo) {
+              this.guardarDocentesAsignaturas[idOld] = docente;
+              this.guardarDocentesAsignaturas[idOld].estado = false;
+              delete this.guardarDocentesAsignaturas[idOld].Persona;
+              delete this.guardarDocentesAsignaturas[idOld].Asignatura;
+            } else {
+              this.guardarDocentesAsignaturas[idOld] = null;
+            }
+          break;
+        }
+        if (this.guardarDocentesAsignaturas[idOld]) {
+          this.guardarDocenteAsignatura(idOld);
+        }
+      } else {
+        this.iconoGuardaDocenteAsignatura[idOld] = true;
+      }
     }
+
   }
 
   applyFilter(event: Event) {
-    console.log(event.target);
+
     const filterValue = (event.target as HTMLInputElement).value;
-    console.log(filterValue);
+    const filtro = filterValue.trim().toLowerCase();
     if (filterValue === '') {
       this.docentes = this.docentesOrg;
     } else {
-      this.docentes = this.docentesOrg.filter(docente => docente.apellidoPaterno.includes(filterValue));
+      this.docentes = this.docentesOrg.filter(docente => {
+        const apellidoPaterno = docente.apellidoPaterno.trim().toLowerCase();
+        const apellidoMaterno = docente.apellidoMaterno.trim().toLowerCase();
+        const primerNombre = docente.primerNombre.trim().toLowerCase();
+        const segundoNombre = docente.segundoNombre.trim().toLowerCase();
 
-      // mejorar el filtro
+        if (apellidoPaterno.includes(filtro) || apellidoMaterno.includes(filtro) ||
+        primerNombre.includes(filtro) || segundoNombre.includes(filtro)) {
+          return true;
+        }
+      });
     }
   }
 
+  ordenarNombre() {
+    if (this.ordNombre) {
+      this.docentes.sort((a, b) => {
+        if (a.apellidoPaterno < b.apellidoPaterno) {
+          return -1;
+        } else if (a.apellidoPaterno > b.apellidoPaterno) {
+          return 1;
+        } else {
+          if (a.apellidoMaterno < b.apellidoMaterno) {
+            return -1;
+          } else if (a.apellidoMaterno > b.apellidoMaterno) {
+            return 1;
+          } else {
+            if (a.primerNombre < b.primerNombre) {
+              return -1;
+            } else if (a.primerNombre > b.primerNombre) {
+              return 1;
+            } else {
+              if (a.segundoNombre < b.segundoNOmbre) {
+                return -1;
+              } else if (a.segundoNombre > b.segundoNombre) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          }
+        }
+      });
+    } else {
+      this.docentes.sort((a, b) => {
+        if (b.apellidoPaterno < a.apellidoPaterno) {
+          return -1;
+        } else if (b.apellidoPaterno > a.apellidoPaterno) {
+          return 1;
+        } else {
+          if (b.apellidoMaterno < a.apellidoMaterno) {
+            return -1;
+          } else if (b.apellidoMaterno > a.apellidoMaterno) {
+            return 1;
+          } else {
+            if (b.primerNombre < a.primerNombre) {
+              return -1;
+            } else if (b.primerNombre > a.primerNombre) {
+              return 1;
+            } else {
+              if (b.segundoNombre < a.segundoNombre) {
+                return -1;
+              } else if (b.segundoNOmbre > a.segundoNombre) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          }
+        }
+      });
+    }
+    this.ordNombre = !this.ordNombre;
+  }
 
+  ordenarHora() {
+    this.ordHora ? this.docentes.sort((a, b) => a.horas - b.horas) : this.docentes.sort((a, b) => b.horas - a.horas);
+    this.ordHora = !this.ordHora;
+  }
 }
